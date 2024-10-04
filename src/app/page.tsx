c41a3d41cc58057024736c14c40b2925dc53406b
fgshw <161293522+fgshw@users.noreply.github.com>
2024-10-04 19:28:05 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"; // เพิ่มบรรทัดนี้ที่ด้านบนสุดของไฟล์
 
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import './styles.css'; // นำเข้า CSS
 
 interface Todo {
